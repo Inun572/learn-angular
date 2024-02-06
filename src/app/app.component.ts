a9a1141f593d2c731fcfe921d7ef36a3dd14fe0e
Inun572 <helmi.ainun@gmail.com>
2024-02-06 10:49:16 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'hello-world';
+  title = 'Angular';
+  count = 0;
+
+  todos: string[] = ['eat', 'sleep', 'code', 'repeat'];
+  newTodo = '';
+
+  increment() {
+    this.count += 1;
+  }
+
+  decrement() {
+    if (this.count <= 0) return;
+    this.count -= 1;
+  }
+
+  addTodo(e: KeyboardEvent) {
+    if (this.newTodo === '') return;
+    if (e.key === 'Enter') {
+      this.todos.push(this.newTodo);
+      this.newTodo = '';
+    }
+  }
+
+  onInputChange(event: Event) {
+    this.newTodo = (event.target as HTMLInputElement).value;
+  }
 }
